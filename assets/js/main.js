6821ee9229e28fcfd51ef480461604ec06e23017
ariefkahfi <ariefkahfi@gmal.com>
2018-11-25 04:15:22 +0700

    search docter

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,16 +29,114 @@ $('#content').pagepiling({
   }
 });
 
-// API KEY
+// Google map API KEY
 // AIzaSyDnvZnMUERWi8IQJkiI9Dj3hPjAjgCopus
 
-// function initMap(){
-//   var map;
-//   map = new google.maps.Map(document.getElementById('map'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 8
-//   });
-// }
+// Google geolocation API KEY
+// AIzaSyC7TsXuBXKU53brJn5sQSlXzjk-yOx-WOA
+
+var markers = [];
+
+var map = null;
+
+function initMap(){
+
+  var uluru = {lat: -25.344, lng: 131.036};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: uluru
+  });
+
+
+  var autoCompleteInputEl = document.getElementById('autoCompleteInput');
+  var autocomplete = new google.maps.places.Autocomplete(autoCompleteInputEl);
+
+  
+  autocomplete.addListener('place_changed',function(){
+      var place = autocomplete.getPlace();
+
+      var location = place.geometry.location;
+      
+      var lat = location.lat();
+      var lng = location.lng();
+
+      map.setCenter({
+        lat:lat,
+        lng:lng
+      });
+      map.setZoom(8);
+      deleteAllMarkers();
+
+      searchNearbyPlaces(location, 'doctor');
+  });
+}
+
+
+function setAllMap(currVal){
+
+  markers.forEach(function(m){
+      m.setMap(currVal);
+  });
+}
+
+function addAllMarkers(arrOfLocation) {
+
+  arrOfLocation.forEach(function(a){
+      var newMarkers = new google.maps.Marker({
+        position: a.latlng,
+        map: map,
+        title: a.name
+      });
+      markers.push(newMarkers);
+  });
+}
+
+function deleteAllMarkers(){
+    setAllMap(null);
+    markers = [];
+}
+
+function searchNearbyPlaces(location, typeOfPlaces){
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location:new google.maps.LatLng(
+      location.lat(),
+      location.lng()
+    ),
+    radius:'1000',
+    type:[typeOfPlaces]
+  }, onGetResponse);
+}
+
+function onGetResponse(results, status) {
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    var mappedResult = results.map(function(r){
+      return {
+        latlng:new google.maps.LatLng(
+          r.geometry.location.lat(),
+          r.geometry.location.lng()
+        ),
+        name:r.name
+      }
+    });
+    
+    addAllMarkers(mappedResult);
+  }
+}
+
+
+function reinitMap(){
+  $('#autoCompleteInput').val('');
+  deleteAllMarkers();
+  var uluru = {lat: -25.344, lng: 131.036};
+
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: uluru
+  });
+
+}
 
 function cariDokter() {
   $('#modal-wrapper').show();
@@ -46,6 +144,7 @@ function cariDokter() {
 
 $(window).click(function(ev){
   if(ev.target.id === 'modal-wrapper') {
-    $('#modal-wrapper').hide();
+      $('#modal-wrapper').hide();
+      reinitMap();
   }
 });
