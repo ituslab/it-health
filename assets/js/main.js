04ffa3df330a699b24d38f91e088a1aee2aaad5a
Sutan Nst <sutan.gnst@gmail.com>
2018-11-24 20:52:28 +0700

    modified animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 $(window).on('load', function() {
   setTimeout(function() {
     $('body').removeClass('disable-scroll');
-    $('.preloader-container').fadeOut('slow');
+    $('.preloader-container').fadeOut('slow', function() {
+      addAnimation('#section-1');
+    });
   }, 500);
 })
 
@@ -9,6 +11,11 @@ $('#content').pagepiling({
   anchors: ['section-1', 'section-2', 'section-3', 'section-4'],
 	afterLoad: function(anchorLink, index){
     addAnimation('#'+anchorLink);
+    setTimeout(function() {
+      if (anchorLink != 'section-1') {
+        removeAnimation('#section-1');
+      }
+    }, 500)
   },
   onLeave: function (index, nextIndex, direction) {
     removeAnimation('#section-'+index);
